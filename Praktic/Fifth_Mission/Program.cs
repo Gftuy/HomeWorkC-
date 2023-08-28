20937c4a7242c099c07d36b148e9882c5fa20e83
serejaARKH <112751142+serejaARKH@users.noreply.github.com>
2023-08-28 10:21:56 +0300

    2.0

--- Praktic/Fifth_Mission/Program.cs
+++ Praktic/Fifth_Mission/Program.cs
@@ -12,43 +12,38 @@ namespace Fifth_Mission
         static void Main()
         {
             Random rand = new Random();
-
-            Console.WriteLine("Если вы хотите выйти из игры введите 0");
-            Console.WriteLine("какой диапазон? ");
+            Console.WriteLine("Если вы хотите выйти из игры, просто нажмите Enter.");
+            Console.WriteLine("Какой диапазон? ");
             int value = int.Parse(Console.ReadLine());
-            int vau;
-            int number = rand.Next(value);
-                                                                  //Я так и не понял как реализовать строчку 5 в задание
-            for (int i = 1; i < value; i++)                       // только смог реализовать с 0
+            int number = rand.Next(1, value + 1);
+            while (true)
             {
-                while (1 == 1)
+                Console.WriteLine("\nВведите число (или просто нажмите Enter для выхода): ");
+                string input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
                 {
-                    Console.WriteLine("\nВведите число: ");
-                    vau = Convert.ToInt32(Console.ReadLine());
-                    if (vau == 0)
-                    {
-                        Console.WriteLine($"Вы проиграли \nБыло загаданное число {number} ");
-                        Console.ReadLine();
-                        break;
-                    }
-                    if (number < vau)
-                    {
-                        Console.WriteLine("Введенное число меньше загаданного");
-                    }
-                    else if (number > vau)
-                    {
-                        Console.WriteLine("Введенное число больше загаданного");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Поздравляю вы угадали");
-                        Console.ReadKey();
-                        break;
-                    }
+                    Console.WriteLine($"Вы проиграли\nБыло загаданное число {number}");
+                    break;
                 }
-                break;
+                else if (!int.TryParse(input, out int vau) || vau < 1 || vau > value)
+                {
+                    Console.WriteLine($"Введите число от 1 до {value} или оставьте поле пустым для выхода.");
+                }
+                else if (vau < number)
+                {
+                    Console.WriteLine("Введенное число меньше загаданного.");
+                }
+                else if (vau > number)
+                {
+                    Console.WriteLine("Введенное число больше загаданного.");
+                }
+                else
+                {
+                    Console.WriteLine("Поздравляю, вы угадали!");
+                    break;
+                }
             }
-
+            Console.ReadKey();
         }
     }
 }
