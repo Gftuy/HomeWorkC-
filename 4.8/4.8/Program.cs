c905c625df17cf3b2fda081caf1f45c5aa2caaf6
serejaARKH <112751142+serejaARKH@users.noreply.github.com>
2023-08-31 15:39:18 +0300

    4.8

---
+++ 4.8/4.8/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество строк в будущей матрице: ");
+            int line = int.Parse(Console.ReadLine());
+            Console.Write("Введите количество столбцов в будущей матрице: ");
+            int column = int.Parse(Console.ReadLine());
+
+            int[,] arr = new int[line, column]; 
+            int sum = 0;
+
+            Random rand = new Random();
+
+            for (int i = 0; i < line; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    arr[i, j] = rand.Next(100);
+                    Console.Write($"{arr[i, j]} ");
+                    sum += arr[i , j];
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine($"Сумма всех чисел равна = {sum}");
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ 4.8/Second_Missions/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Second_Missions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество строк в будущей матрице: ");
+            int line = int.Parse(Console.ReadLine());
+            Console.Write("Введите количество столбцов в будущей матрице: ");
+            int column = int.Parse(Console.ReadLine());
+
+            int[,] arrA = new int[line, column];
+            int[,] arrB = new int[line, column];
+            int[,] arrC = new int[line, column];
+
+            Random rand = new Random();
+            Console.WriteLine("1-я матрица:");
+            for (int i = 0; i < line; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    arrA[i, j] = rand.Next(-10 , 10);
+                    Console.Write($"{arrA[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("2-я матрица:");
+            for (int i = 0; i < line; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    arrB[i, j] = rand.Next(-10 , 10);
+                    Console.Write($"{arrB[i, j]} ");
+                    
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine($"Сложенние матриц:");
+            for (var i = 0; i < line; i++)
+            {
+                for (var j = 0; j < column; j++)
+                {
+                    arrC[i, j] = arrA[i, j] + arrB[i, j];
+                    Console.Write($"{arrC[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
