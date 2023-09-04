d5cda86f2224aa5d5da438af5c562819deca54e2
serejaARKH <112751142+serejaARKH@users.noreply.github.com>
2023-09-04 22:55:16 +0300

    5.5

---
+++ 5.5/5.5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace _5._5
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Разделяет предложение на слова.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        static string[] SplitText(string text)
+        {
+            return text.Split(' '); 
+        }
+
+        /// <summary>
+        /// Вывод в строчку (слово)
+        /// </summary>
+        /// <param name="textSplit"></param>
+        static void Vivod(string[] textSplit)
+        {
+            foreach (string word in textSplit)
+            {
+                Console.WriteLine(word);
+            }
+        }
+        /// <summary>
+        /// Выводит 2 метода на экран
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите предложение. ");
+            Vivod(SplitText(Console.ReadLine()));
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ 5.5/Second_Mission/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Second_Mission
+{
+    internal class Program
+    {
+       
+        /// <summary>
+        /// Разделяет предложение на слова.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        static string[] SplitText(string text)
+        {
+            return text.Split(' ');
+        }
+        /// <summary>
+        /// меняет слова заданом на перед.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        static string Reverse(string text)
+        {
+            string ReversString = null;
+            foreach (var word in SplitText(text).Reverse())
+            {
+                ReversString += word + " ";
+            }
+            return ReversString;
+        }
+        /// <summary>
+        /// Вывод и Ввод  
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение. ");
+            string reversWord = Reverse(Console.ReadLine());
+            Console.WriteLine(reversWord);
+            Console.ReadKey();
+        }
+    }
+}
